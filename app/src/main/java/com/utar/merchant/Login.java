6883796198745053bfdab518c6c1f873cd95351a
Yao <cyaoen@gmail.com>
2023-03-06 22:03:52 +0800

    Initial Commit Finish: - Login - Registration

---
+++ app/src/main/java/com/utar/merchant/Login.java
@@ -0,0 +1,106 @@
+package com.utar.merchant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+
+    EditText editTextEmail, editTextPassword;
+    Button btnLogin;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView tvLoginPage;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+        btnLogin = findViewById(R.id.btn_login);
+
+        progressBar = findViewById(R.id.progressBar);
+        tvLoginPage = findViewById(R.id.registerNow);
+
+        tvLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText().toString());
+
+                if(email.isEmpty()){
+                    toast("Enter email");
+                    return;
+                }
+
+                if(password.isEmpty()){
+                    toast("Enter password");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    toast("Login Successful");
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    private void toast(String msg){
+        Toast.makeText(Login.this, msg, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/utar/merchant/MainActivity.java
@@ -0,0 +1,49 @@
+package com.utar.merchant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button btnLogout;
+    TextView tv;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        btnLogout = findViewById(R.id.logout);
+        tv = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+
+        if(user == null){
+            startActivity(new Intent(getApplicationContext(), Login.class));
+            finish();
+        }
+        else{
+            tv.setText(user.getEmail());
+        }
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
+    }
+}
